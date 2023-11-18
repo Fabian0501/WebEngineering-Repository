93986f47a3a004a949da114d7cdbbbcdede94add
Fabian.R <ratschuweit0501@gmail.com>
2023-11-18 10:53:17 +0100

    eine zweite sidebar hinzugefügt

--- Code/solutionPage/javaScript.js
+++ Code/solutionPage/javaScript.js
@@ -1,30 +1,31 @@
 
 /*sidebar for KW*/
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openSidebar() {
+/* Set the width of the sidebar to 200px and the left margin of the page content to 200px */
+function openKWSidebar() {
     document.getElementById("sidebarKW").style.width = "200px";
     document.getElementById("main").style.marginLeft = "200px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeSidebar() {
+function closeKWSidebar() {
     document.getElementById("sidebarKW").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
 
 
 /*sidebar for important exam Tasks*/
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openSidebar() {
-    document.getElementById("mySidebar").style.width = "200px";
-    document.getElementById("main").style.marginLeft = "200px";
+/* Set the width of the sidebar to 300px and the left margin of the page content to 300px */
+function openTasksSidebar() {
+    document.getElementById("sidebarTasks").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeSidebar() {
-    document.getElementById("mySidebar").style.width = "0";
+function closeTasksSidebar() {
+    document.getElementById("sidebarTasks").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
 
 
 
