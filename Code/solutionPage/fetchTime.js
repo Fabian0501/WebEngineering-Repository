09fb9c50354dd13e65fb51ff5bd8d105f5cada11
Fabian.R <ratschuweit0501@gmail.com>
2024-02-07 15:10:51 +0100

    fetch api mittels time

---
+++ Code/solutionPage/fetchTime.js
@@ -0,0 +1,14 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('http://worldclockapi.com/api/json/utc/now')
+        .then(response => response.json())
+        .then(data => {
+            let currentDate = data.currentDateTime;
+            const timeDiv = document.getElementById("time");
+            timeDiv.innerHTML = `Date: ${currentDate}`;
+        })
+        .catch(error => {
+            console.error('Error fetching current date:', error);
+        });
+});
