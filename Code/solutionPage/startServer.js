84737b76767c5ed6fcebfbd77a884e489a67c553
Fabian.R <ratschuweit0501@gmail.com>
2024-02-07 15:11:03 +0100

    script zum starten des node servers

---
+++ Code/solutionPage/startServer.js
@@ -0,0 +1,8 @@
+// stellt die funktionalität etwas im terminal einzugeben von Node.js-Modul zu verfügung
+function startNodeServer() {
+   const { exec } = require('child_process');
+    const serverPath = 'kw52/server.js';
+    exec(`node ${serverPath}`);
+}
+startNodeServer();
+
